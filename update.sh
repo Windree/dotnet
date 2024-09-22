f856e07e99d5cc09009b9f850a6a72c7ec707547
Windree <windree@dragonsoul.com>
2024-09-22 17:05:45 +0000

    feat: restart docker on update

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 root="$(dirname "${BASH_SOURCE[0]}")"
+
 git -C "$root" pull
 git -C "$root/data/web" reset --hard
 git -C "$root/data/web" pull --ff
+
+docker compose stop && \
+docker compose up --build -d
