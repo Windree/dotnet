233b8a2f9da3c13f7fd627a6a79bb5d34cda4c1d
Windree <windree@dragonsoul.com>
2025-03-03 13:28:34 +0000

    feat: open port for deployment

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+root="$(dirname "${BASH_SOURCE[0]}")"
+source "$root/.env"
+
+function main() {
+    cd "$root"
+    echo "$(date) Start"
+    while true; do
+        echo "$(date) Wating for hook"
+        nc -I 1 -l "$DRAGONSOUL_ART_DEPLOY_PORT" >/dev/null
+        deploy || true
+    done
+}
+
+function deploy() {
+    echo "$(date) Redeploy"
+    docker compose stop &&
+        git -C "data/web" clean -fd &&
+        git -C "data/web" reset --hard &&
+        git -C "data/web" pull --ff &&
+        docker compose up -d || true
+    echo "$(date) Complete"
+}
+
+main | tee -a "$root/deploy.log"
--- ufw.sh
+++ ufw.sh
@@ -2,9 +2,10 @@
 source "$(dirname "${BASH_SOURCE[0]}")/.env"
 set -Eeuo pipefail
 for remote in "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16" "fe80::/64"; do
-    for port in "$HTTP_PORT" "$HTTPS_PORT"; do
+    for port in "$DRAGONSOUL_ART_HTTP_PORT" "$DRAGONSOUL_ART_HTTPS_PORT"; do
         ufw allow from "$remote" to any port "$port" proto tcp
     done
 done
 
+ufw allow $DRAGONSOUL_ART_DEPLOY_PORT
 ufw reload
